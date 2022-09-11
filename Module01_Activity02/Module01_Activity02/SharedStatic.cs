2aa8787b85706695f1671eb994a776c5689ecbfa
Troy <Troy@DESKTOP-JLCV8UI>
2022-09-11 23:27:31 +0800

    Add project files.

---
+++ Module01_Activity02/Module01_Activity02/SharedStatic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Module01_Activity02
+{
+    public class SharedStatic
+    {
+        public static readonly Color ButtonColor = Color.Black;
+        public static readonly Color ButtonTextColor = Color.White;
+
+    }
+}
